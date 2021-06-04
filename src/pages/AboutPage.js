723686706a8c064d1ed19c0f8db6a6e8025749e5
Dilan Ranawaka <dilan.ranawaka99@gmail.com>
2021-06-04 14:47:54 +0530

    Add files

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const AboutPage = () => (
+    <>
+        <h1>About Me..</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Rerum quod deserunt eligendi facere, quia similique! Unde ea earum iusto reiciendis inventore quo,
+            illo pariatur, tempore et excepturi incidunt corrupti aliquam voluptates quidem esse id, 
+            adipisci beatae cumque expedita quos explicabo. 
+            Voluptas, accusantium. Hic eius voluptates distinctio! Molestiae a harum eligendi
+        </p>
+         <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Rerum quod deserunt eligendi facere, quia similique! Unde ea earum iusto reiciendis inventore quo,
+            illo pariatur, tempore et excepturi incidunt corrupti aliquam voluptates quidem esse id, 
+            adipisci beatae cumque expedita quos explicabo. 
+            Voluptas, accusantium. Hic eius voluptates distinctio! Molestiae a harum eligendi
+        </p>
+         <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Rerum quod deserunt eligendi facere, quia similique! Unde ea earum iusto reiciendis inventore quo,
+            illo pariatur, tempore et excepturi incidunt corrupti aliquam voluptates quidem esse id, 
+            adipisci beatae cumque expedita quos explicabo. 
+            Voluptas, accusantium. Hic eius voluptates distinctio! Molestiae a harum eligendi
+        </p>
+    </>
+    
+);
+
+export default AboutPage;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const HomePage = () => (
+    <>
+        <h1>Hello, Welcome!</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Rerum quod deserunt eligendi facere, quia similique! Unde ea earum iusto reiciendis inventore quo, 
+            illo pariatur, tempore et excepturi incidunt corrupti aliquam voluptates quidem esse id, 
+            adipisci beatae cumque expedita quos explicabo. 
+            Voluptas, accusantium. Hic eius voluptates distinctio! Molestiae a harum eligendi
+        </p>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Rerum quod deserunt eligendi facere, quia similique! Unde ea earum iusto reiciendis inventore quo, 
+            illo pariatur, tempore et excepturi incidunt corrupti aliquam voluptates quidem esse id, 
+            adipisci beatae cumque expedita quos explicabo. 
+            Voluptas, accusantium. Hic eius voluptates distinctio! Molestiae a harum eligendi
+        </p>
+    </>
+    
+);
+
+export default HomePage;
